77fae2ecb6a37cc1d5464c3c16265f9a07982970
lidasharova <Lida050496@yandex.ru>
2023-06-04 06:41:51 +0700

    task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -127,8 +127,12 @@ function repeatString(value, count) {
  *   'I like legends', 'end' => 'I like legs',
  *   'ABABAB','BA' => 'ABAB'
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  if (index !== -1) {
+    return str.slice(0, index) + str.slice(index + value.length);
+  }
+  return str;
 }
 
 /**
